280f69f922ac2b8693f17640b30a2b03c4a3fa8b
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2022-08-07 20:02:57 +0000

    close side bar when clicked on closebtn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".menu");
 const navBar = document.querySelector(".nav-bar");
+const closeBtn = document.querySelector(".close");
+
 
 menu.addEventListener("click",()=>{
     navBar.classList.add("active");
     menu.style.display = "none";
-})
+});
+
+closeBtn.addEventListener("click",()=>{
+    navBar.classList.remove("active");
+    menu.style.display = "block";
+});
+
